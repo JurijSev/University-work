1f497bccc04fcfc72ebc257e98b1b4b13e75657b
JurijSev <Jurij.Sevcenko@city.ac.uk>
2021-11-07 21:19:05 +0000

    confirmation

--- InteliJ/Testing/test/src/test.java
+++ InteliJ/Testing/test/src/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("This is a test project to see if the repo has been created correctly");
+        System.out.println("it has");
     }
 }
