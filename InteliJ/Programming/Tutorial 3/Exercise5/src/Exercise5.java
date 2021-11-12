35fe7beb73eb29696f0ac72eb7194e5951bcb560
JurijSev <Jurij.Sevcenko@city.ac.uk>
2021-11-12 00:08:44 +0000

    completed java exercise 5 from 3rd tutorial

--- InteliJ/Programming/Tutorial 3/Exercise5/src/Exercise5.java
+++ InteliJ/Programming/Tutorial 3/Exercise5/src/Exercise5.java
@@ -1,21 +1,26 @@
 public class Exercise5 {
     public static void main(String[] args) {
-//        int a = 1;
-//        int b = 2;
         int[] array = {45,3,12,6};
-
         int meanA = aMean(array);
-        //int meanG = gMean(a,b);
-
+        int meanG = gMean(array);
+        if(meanA>meanG){System.out.println("True");
+        }else{System.out.println("The mean values are equal");}
     }
     public static int aMean(int[] array){
+        int total = 0;
         for(int i = 0;i<array.length;i++){
-            System.out.print(array.get(i));
+            total = total+array[i];
         }
-
-        return 1;
+        int mean = total/ (array.length);
+        return mean;
     }
-//    public static int gMean(int x, int y){
-//
-//    }
+    public static int gMean(int[] array){
+        double total = 1;
+        for(int i = 0;i<array.length;i++){
+            total = total*array[i];
+        }
+        double mean = Math.pow((total),1/(double)array.length);
+        int meanRounded = (int) Math.round(mean);
+        return meanRounded;
+    }
 }
