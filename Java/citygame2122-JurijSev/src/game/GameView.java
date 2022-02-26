8a6b1638cddf58a48d73ac34c53260eaacf4753b
JurijSev <Jurij.Sevcenko@city.ac.uk>
2022-02-26 20:59:04 +0000

    Added correct background - sky and grass.

--- Java/citygame2122-JurijSev/src/game/GameView.java
+++ Java/citygame2122-JurijSev/src/game/GameView.java
@@ -7,15 +7,18 @@ import java.awt.*;
 
 public class GameView extends UserView {
 
-    private Image background;
+    private Image sky;
+    private Image grass;
 
     public GameView(GameWorld world, int width, int height) {
         super(world, width, height);
-        background = new ImageIcon("data/Woods location.png").getImage();
+        sky = new ImageIcon("data/Background/blueSky.jpg").getImage();
+        grass = new ImageIcon("data/Background/Grass.png").getImage();
     }
 
     @Override
     protected void paintBackground(Graphics2D g) {
-        g.drawImage(background, -100, 0,2200,500, this);
+        g.drawImage(sky, 0, 0,500,500, this);
+        g.drawImage(grass, -100, 100,1000,500, this);
     }
 }
