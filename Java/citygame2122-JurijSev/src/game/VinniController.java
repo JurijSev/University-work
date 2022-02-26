825a1aca51bc50fef5ff3c046077049f60de4e62
JurijSev <Jurij.Sevcenko@city.ac.uk>
2022-02-26 20:42:13 +0000

    Vinni now changes image depending on whether he is walking (left and right have their own images) or standing which depends on which way he was walking previously. The images (while moving) now are gifs so now it is an animation.

--- Java/citygame2122-JurijSev/src/game/VinniController.java
+++ Java/citygame2122-JurijSev/src/game/VinniController.java
@@ -1,14 +1,15 @@
 package game;
 
 import city.cs.engine.BodyImage;
+import city.cs.engine.Walker;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class VinniController implements KeyListener {
 
-    //private static final float WALKING_SPEED = 5;
-    Vinni vinni;
+    private static final float WALKING_SPEED = 5;
+    private Vinni vinni;
 
     public VinniController(Vinni v) {
         this.vinni = v;
@@ -19,12 +20,12 @@ public class VinniController implements KeyListener {
         int code = e.getKeyCode();
         if(code == KeyEvent.VK_D){
             vinni.removeAllImages();
-            vinni.addImage(new BodyImage("data/Vinni/11 right.png", 4f));
-            vinni.startWalking(5);
+            vinni.addImage(new BodyImage("data/Vinni/VinniRightWalk.gif", 4f));
+            vinni.startWalking(WALKING_SPEED);
         }else if(code == KeyEvent.VK_A){
             vinni.removeAllImages();
-            vinni.addImage(new BodyImage("data/Vinni/11.png", 4f));
-            vinni.startWalking(-5);
+            vinni.addImage(new BodyImage("data/Vinni/VinniLeftWalk.gif", 4f));
+            vinni.startWalking(-WALKING_SPEED);
         }else if(code == KeyEvent.VK_SPACE){
             vinni.jump(10);
         }
